c888023a276d3195a8fe7060e8e30e83693ccd59
Phill <fotofill.holland@googlemail.com>
2023-05-11 15:41:14 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Diagnostics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System.Net;
+
+string endPoint = "https://api.honeycomb.io:443";
+string apiKey = "MyApiKey";
+string serviceName = "MyServiceName";
+
+OpenTelemetry.Exporter.OtlpExportProtocol protocol = 
+    OpenTelemetry.Exporter.OtlpExportProtocol.Grpc;
+
+Console.WriteLine("Usage: opentelemetry-data-generator endpoint apikey servicename mode");
+
+if(args.Length > 0) endPoint = args[0];
+if(args.Length > 1) apiKey = args[1];
+if(args.Length > 2) serviceName = args[2];
+if(args.Length > 3)
+{
+    if(args[3].CompareTo("HTTP") == 0)
+    {
+        protocol = OpenTelemetry.Exporter.OtlpExportProtocol.HttpProtobuf;
+        string[] values = endPoint.Split('/');
+        if(values.Length < 5) endPoint = endPoint + "/v1/traces";
+        else 
+        {
+            if((values[values.Length -2] != "v1")&&(values[values.Length - 1]!= "traces"))
+            {
+                endPoint = endPoint + "/v1/traces";
+            }
+        }
+    }
+}
+
+Console.WriteLine($"opentelemetry-data-generator {endPoint} {apiKey} {serviceName}");
+
+ActivitySource activitySource = new ActivitySource(serviceName);
+
+using var tracerProvider = OpenTelemetry.Sdk.CreateTracerProviderBuilder()
+                .AddSource(serviceName)
+                .AddAspNetCoreInstrumentation()
+                .AddHttpClientInstrumentation()   
+                .AddConsoleExporter()                
+                .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName)) 
+                .AddOtlpExporter(option =>
+                {
+                    option.Endpoint = new Uri(endPoint);
+                    option.Headers = $"X-Honeycomb-Team={apiKey}";
+                    option.Protocol = protocol;
+                })      
+                .SetErrorStatusOnException()      
+                .Build();
+
+Tracer tracer = tracerProvider.GetTracer(serviceName);
+
+const int iterations = 50;
+
+string []words = { "banana", "giraffe", "cheese", "monkey", "cat", "badger", "apple" };
+for(int i = 0; i < iterations; ++i)
+{
+    HttpClient client = new HttpClient();    
+    if(i % 10 == 0)
+    {
+        // intentionally generator a 404 page not found error
+        await client.GetAsync("http://www.google.co.uk/seaffh?q=vanilla");
+    }
+    else
+    {
+        // generate a search from google
+        await client.GetAsync("http://www.google.co.uk/search?q=" + words[i % words.Length]);
+    }
+}
---
+++ README.md
@@ -0,0 +1,53 @@
+<b>OpenTelemetry Data Generator Console Application</b>
+
+This application demonstrates the simple setup and initalisation of an dotnet core based OpenTelemetry console application, and then generates some dummy data Open Telemtery traces, which can be configured to send to your collection of choice, such as Honeycomb and/or Jaeger.
+
+This application has a number command line arguments to enable this use;
+
+arg1
+Endpoint to your collector, such as https://api.honeycomb.io:443
+
+arg2 
+ApiKey
+
+arg3 
+ServiceName (or DataSet name) - can be anything!
+
+arg4
+GRCP or HTTP
+
+If you do not put in these values, it'll resort to defaults, however the defaults will not work!
+
+An application such as this is useful for testing quickly network configuration issues whilst deploying OpenTelemetry inside larger production applications.
+
+Also included by default, is an OTEL_DIAGNOSTICS.json file,
+which enables the OpenTelemetry packages to generate further logfiles in the working directory, to help diagnose additional Open Telemetry issues
+
+<ul>
+<li><b>Ensure project is open within the development container</b></li>
+<li><b>Hit F5</b></li>
+</ul>
+
+This application demonstrates the following;
+
+<ul>
+<li>VS code debugging and breakpoint functionality</li>
+<li>Docker development container configuration</li>
+</ul>
+
+Requirements (optional);
+
+The VSCode development container plugin is installed;
+
+https://code.visualstudio.com/docs/remote/containers
+
+Docker must also be installed;
+
+https://docs.docker.com/get-docker/
+
+This application, however is configured with linux based containers, and will not work correctly on Windows without modification.
+
+For further information, go to;
+
+https://dotnet.microsoft.com/download
+
